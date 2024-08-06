568f71cd287156a8c6b147d3520ac395e64a690d
parham <parham@datastack.academy>
2024-08-06 18:18:33 -0400

    functions, loops, and objects lessons

---
+++ src/lessons/240806_functions_loops_objects.js
@@ -0,0 +1,56 @@
+/*
+functions, loops, and objects
+*/
+
+function normalGreet(name) {
+    return `my name is ${name}`;
+}
+
+// arrow function
+let arrowGreet = (name, count = 2) => {
+    if (count <= 0) {
+        return '';
+    } else if (count >= 1) {
+        return `my name is ${String(name).repeat(count)}`;
+    }
+}
+
+let name = 'Sina';
+let func_call = normalGreet(normalGreet(name));
+console.log(func_call);
+name = 'Nima';
+console.log(func_call);
+
+console.log(arrowGreet('Parham', 5))
+
+
+// loops
+
+for (i=0; i < 5; i++) {
+    console.log(`i is ${i}`);
+}
+
+let obj1 = {name: 'Parham', age: 42};
+for (k in obj1) {
+    console.log(`obj key: ${k} value is ${obj1[k]}`);
+}
+
+// objects w. methods
+
+let person = {
+    name: 'Parham',
+    age: 42,
+    sayHi: function() {
+        console.log(`hi my name is ${this.name} and I am ${this.age} years old`);
+    },
+    jump: function(count) {
+        console.log(`hi my name is ${this.name} and I jump ${count} times!`);
+    }
+}
+
+person.sayHi();
+person.jump(5);
+person.name = 'Mahsa';
+person.sayHi();
+
+
