3db0183cba786ae95d2545a84fe79b8b2bffef80
parvister <parham@tura.io>
2024-08-14 00:02:29 -0400

    added an exercise with Car and ElectricCar class.

--- lessons/20240812_classes.js
+++ lessons/20240812_classes.js
@@ -2,6 +2,8 @@
  * practicing classes and OOP
  */
 
+const { useSyncExternalStore } = require("react");
+
 class Rectangle {
     #width; // Private field
     #height; // Private field
@@ -60,3 +62,85 @@ rect.width = 10;
 console.log(`area: ${rect.area} circumference: ${rect.circumference}`);
 // rect.width = -10;
 rect.greet();
+
+
+class Car {
+    #make;
+    #model;
+    #year;
+
+    constructor(make, model, year) {
+        this.#make = make;
+        this.#model = model;
+        const currentYear = new Date().getFullYear();
+        if (Number.isInteger(year) && (year >= 1890) && (year <= (currentYear + 1))) {
+            this.#year = year;
+        } else {
+            throw new Error(`Year must be an integer greater than 1890 and ${currentYear + 1}. Provided value: ${year}`);
+        }
+    }
+
+    get make() {
+        return this.#make;
+    }
+
+    set make(value) {
+        this.#make = value;
+    }
+
+    get model() {
+        return this.#model;
+    }
+
+    set model(value) {
+        this.#model = value;
+    }
+
+    get year() {
+        return this.#year;
+    }
+
+    set year(value) {
+        const currentYear = new Date().getFullYear();
+        if (Number.isInteger(value) && (value >= 1890) && (value <= (currentYear + 1))) {
+            this.#year = value;
+        } else {
+            throw new Error(`Year must be an integer greater than 1890 and ${currentYear + 1}. Provided value: ${value}`);
+        }
+    }
+
+    getDetails() {
+        return `A ${this.#year} ${this.#make} ${this.#model}`;
+    }
+}
+
+class ElectricCar extends Car {
+    #range;
+
+    constructor(make, model, year, range) {
+        super(make, model, year);
+        this.#range = range;
+    }
+
+    get range() {
+        return this.#range;
+    }
+
+    set range(value) {
+        this.#range = value;
+    }
+
+    getDetails() {
+        return `${super.getDetails()} with range of ${this.#range}`;
+    }
+
+    static isElectric() {
+        return true;
+    }
+}
+
+let camry = new Car('Toyota', 'Camry', 1995);
+console.log(camry.getDetails());
+
+let tesla = new ElectricCar('Tesla', 'Model X', 2024, 250);
+console.log(tesla.getDetails());
